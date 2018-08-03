163e2b55ee5bfe8a4d7843d4011e5c4ed1c5656d
jeremypagley <jeremyjkp@gmail.com>
2018-08-02 20:10:56 -0400

    Add search to order container

--- app/containers/Order.js
+++ app/containers/Order.js
@@ -1,5 +1,20 @@
 import React from 'react';
-import { Container, Text, List, ListItem, Content, Title, Header, Right, Body, Left } from 'native-base';
+import { 
+  Container,
+  Text,
+  List,
+  ListItem,
+  Content,
+  Title,
+  Header,
+  Right,
+  Body,
+  Form,
+  Input,
+  Item,
+  Label
+} from 'native-base';
+import { Col, Row, Grid } from 'react-native-easy-grid';
 import { View } from 'react-native';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
@@ -7,6 +22,24 @@ import gql from 'graphql-tag';
 import ContainerStyles from 'app/styles/generic/ContainerStyles.js'
 
 class Order extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      search: '',
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }
+    }
+  }
+  
+  onRegionChange(region) {
+    this.setState({ region });
+  }
+  
   handleLogout = () => {
     return this.props.screenProps.changeLoginState(false);
   };
@@ -21,37 +54,68 @@ class Order extends React.Component {
           <Right />
         </Header>
 
-        <Content>
-          <Query query={StoresQuery}>
-            {({ loading, error, data }) => {
-              if (loading) return <Text key="loading">Loading...</Text>;
-              if (error) return <Text key="error">Error :(</Text>;
+        <Grid>
+          <Row size={1}>
+            <Col>
+              <Form>
+                <Item floatingLabel>
+                  <Label>Search location by title</Label>
+                  <Input
+                    clearButtonMode="always"
+                    value={this.state.search}
+                    onChangeText={(text) => this.setState({ search: text })}
+                  />
+                </Item>
+              </Form>
+            </Col>
+          </Row>
+          <Row size={6}>
+            <Content>
+              <Query query={StoresQuery}>
+                {({ loading, error, data }) => {
+                  if (loading) return <Text key="loading">Loading...</Text>;
+                  if (error) return <Text key="error">Error :(</Text>;
 
-                console.log(data)
-              return (
-                <List>
-                  <ListItem itemHeader first>
-                    <Text>FOUND</Text>
-                  </ListItem>     
-                  {data.stores.map(({ _id, title, location }) => {
-                    return (
-                      <ListItem key={_id}>
-                        <Body>
-                          <Text>{title}</Text>
-                          <Text note>{location.latitude} - {location.longitude}</Text>
-                        </Body>
-                      </ListItem>
-                    )
-                  })}
-                </List>
-              )
+                  return (
+                    <List>
+                      <ListItem itemHeader first>
+                        <Text>FOUND</Text>
+                      </ListItem>     
+                      {this.getListItems(data)}
+                    </List>
+                  )
+                }}
+              </Query>
+            </Content>
+          </Row>
+        </Grid>
 
-            }}
-          </Query>
-        </Content>
+       
       </Container>
     );
   }
+
+  getListItems = ({ stores }) => {
+    const { search } = this.state;
+
+    if (search !== '') {
+      stores = stores.filter(store => {
+        return store.title.toLowerCase().indexOf(search.toLowerCase()) != -1 ? store : null
+      });
+    }
+
+    return stores.map(({ _id, title, location }) => {
+      return (
+        <ListItem key={_id}>
+          <Body>
+            <Text>{title}</Text>
+            <Text note>{location.address}</Text>
+          </Body>
+        </ListItem>
+      )
+    })
+  }
+
 }
 
 const StoresQuery = gql`
@@ -64,8 +128,7 @@ const StoresQuery = gql`
     phone,
     website,
     location {
-      latitude,
-      longitude
+      address
     },
     productCategories {
       title,
