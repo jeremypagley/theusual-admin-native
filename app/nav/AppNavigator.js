1cc7ef7ecb4de9ba181465aa24910d95618d96ca
jeremypagley <jeremyjkp@gmail.com>
2018-10-10 00:21:58 -0400

    Update footer

--- app/nav/AppNavigator.js
+++ app/nav/AppNavigator.js
@@ -1,57 +1,67 @@
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
 
-import Profile from 'app/scenes/Profile';
+import Cart from 'app/scenes/Profile';
+import Settings from 'app/scenes/Profile';
 import Order from 'app/scenes/Order';
 import Store from 'app/scenes/Store';
 import Products from 'app/scenes/Products';
 import Product from 'app/scenes/Product';
 import Usuals from 'app/scenes/Usuals';
-import { Entypo } from '@expo/vector-icons';
+import { Icon } from 'native-base';
+
+import Colors from 'app/styles/Colors';
 
 import React from 'react';
 import { Text } from 'native-base';
 
-const HomeStack = createStackNavigator({
+const UsualsStack = createStackNavigator({
   Usuals: { screen: Usuals },
-},
-{
-  headerMode: 'none',
 });
 
-const OrderStack = createStackNavigator({
+const StoresStack = createStackNavigator({
   Order: { screen: Order },
   Store: { screen: Store },
   Products: { screen: Products },
   Product: { screen: Product },
-},
-{
-  headerMode: 'none',
 });
 
 const AppStack = createBottomTabNavigator({
-  Home: HomeStack,
-  Order: OrderStack,
-  Profile: Profile,
+  Usuals: UsualsStack,
+  Stores: StoresStack,
+  Cart: Cart,
+  Settings: Settings,
 },
 {
-  headerMode: 'none',
-  tabBarOptions: {
-    // showLabel: false
-  },
+  headerMode: 'float',
   navigationOptions: ({ navigation }) => ({
+    tabBarOptions: {
+      activeTintColor: Colors.BrandRed,
+      inactiveTintColor: Colors.BrandBlueGrey,
+      showLabel: false
+    },
     tabBarIcon: ({ focused, tintColor }) => {
       const { routeName } = navigation.state;
       let iconName;
-      if (routeName === 'Home') {
-        iconName = 'home';
-      } else if (routeName === 'Order') {
-        // iconName = `ios-options${focused ? '' : '-outline'}`;
-        iconName = 'shopping-bag';
-      } else if (routeName === 'Profile') {
-        iconName = 'user';
+      if (routeName === 'Usuals') {
+        iconName = 'md-cafe';
+      } else if (routeName === 'Stores') {
+        iconName = 'md-search';
+      } else if (routeName === 'Cart') {
+        iconName = 'md-cart';
+      } else if (routeName === 'Settings') {
+        iconName = 'md-menu';
       }
 
-      return <Entypo name={iconName} size={25} color={tintColor} />;
+      const color = focused ? Colors.BrandRed : Colors.BrandBlueGrey;
+
+      return (
+        <Icon
+          name={iconName}
+          size={22}
+          style={{ marginBottom: -3, color }}
+          active={focused}
+        />
+      )
     },
     
   }),
--- app/styles/Colors.js
+++ app/styles/Colors.js
@@ -6,4 +6,18 @@ export default {
   LightGrey: 'lightgrey',
   White: '#FFFFFF',
   Black: '#212529',
+
+  // Brand New Colors
+  BrandDarkGrey: '#989898',
+  BrandGrey: '#D8D8D8',
+  BrandLightGrey: '#F9F9F9',
+  BrandBlack: '#1C1C1C',
+  BrandGreen: '#89E0B3', 
+  BrandYellow: '#F9DD71',
+  BrandOrange: '#F8BB94',
+  BrandRed: '#F95863',
+  BrandLightRed: '#F9A7AC',
+  BrandDarkBlue: '#323643',
+  BrandBlueGrey: '#B9CEE0',
+  BrandBorderGrey: '#EBEEF1'
 }
